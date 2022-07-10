090603d7ad162825fe7fc08726a446a915bd6c29
Dmitrij <dmitry.bermas@gmail.com>
2022-07-10 22:03:08 +0200

    fixed gallery

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,6 @@ function scripts() {
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
     'app/js/jquery.magnific-popup.min.js',
-    // 'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
